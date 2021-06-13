691fc943f43dc851ea8004e01da83ff530cacd39
meerim05 <meerimt005@gmail.com>
2021-06-13 13:09:15 -0400

    final red audi car at your service

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         Car audi = new Car();
+        audi.color="Red";
+        audi.number="31948";
+        audi.door="2";
+        audi.model="A3";
 
 
     }
